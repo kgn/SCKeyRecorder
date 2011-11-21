9529370dc868505a5ca72322cfaaa4ebea3546cf
David Keegan <git@inscopeapps.com>
2011-11-21 15:36:29 -0800

    initial commit of empty project

---
+++ SCKeyRecorderSample/SCAppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  SCAppDelegate.h
+//  SCKeyRecorderSample
+//
+//  Created by David Keegan on 11/21/11.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface SCAppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ SCKeyRecorderSample/SCAppDelegate.m
@@ -0,0 +1,22 @@
+//
+//  SCAppDelegate.m
+//  SCKeyRecorderSample
+//
+//  Created by David Keegan on 11/21/11.
+//
+
+#import "SCAppDelegate.h"
+
+@implementation SCAppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification{
+    // Insert code here to initialize your application
+}
+
+@end
